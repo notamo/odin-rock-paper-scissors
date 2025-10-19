891421e49abcdf6eb5f4f3f0776a02475e807980
notamo <203415281+notamo@users.noreply.github.com>
2025-10-19 19:42:25 +0200

    Implement logic for single round

--- script.js
+++ script.js
@@ -32,3 +32,65 @@ let getHumanChoice = function () {
 // 2. Initialize those variables with the value of 0.
 let humanScore = 0;
 let computerScore = 0;
+
+// Write the logic to play a single round
+// 1. Create a new function named playRound.
+// 2. Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to 
+//    take the human and computer choices as arguments.
+// 3. Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// 4. Write the code for your playRound function to console.log a string value representing the round winner,
+//    such as: “You lose! Paper beats Rock”.
+// 5. Increment the humanScore or computerScore variable based on the round winner.
+let humanWins    = (winningPick, losingPick) => {
+    console.log(`You win! ${winningPick} beats ${losingPick}`);
+    humanScore++;
+};
+let computerWins = (winningPick, losingPick) => {
+    console.log(`You lose! ${winningPick} beats ${losingPick}`);
+    computerScore++;
+};
+
+let playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw! No points were gained")
+        return
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanWins("paper", "rock");
+        } else {
+            computerWins("scissors", "paper");
+        }
+        return
+    }
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanWins("rock", "scissors");
+        } else {
+            computerWins("paper", "rock");
+        }
+        return
+    }
+    
+    if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanWins("scissors", "paper");
+        } else {
+            computerWins("rock", "scissors");
+        }
+        return
+    }
+    
+    console.log("Invalid human choice! Round cancelled")
+    return
+}
+
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);

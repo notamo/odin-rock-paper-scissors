f3b6f4f407f02e5e37860f63ef3982e5835e86a3
notamo <203415281+notamo@users.noreply.github.com>
2025-10-19 18:55:30 +0200

    Implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// 1. Create a new function named getComputerChoice.
+// 2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
+
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());

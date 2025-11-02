c16f3a2df6f0171dbc19d300a9d8ade73a51fb47
notamo <203415281+notamo@users.noreply.github.com>
2025-11-02 13:40:59 +0100

    Display score and announce winner

--- script.js
+++ script.js
@@ -15,110 +15,108 @@ let getHumanChoice = function () {
     return choice;
 }
 
-let humanWins    = (winningPick, losingPick) => {
-    console.log(`You win! ${winningPick} beats ${losingPick}`);
-    humanScore++;
-};
-let computerWins = (winningPick, losingPick) => {
-    console.log(`You lose! ${winningPick} beats ${losingPick}`);
-    computerScore++;
-};
-
-let playRound = (humanChoice, computerChoice) => {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-
-    if (humanChoice === computerChoice) {
-        console.log("It's a draw! No points were gained");
-        return;
-    }
-
-    if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-            humanWins("paper", "rock");
-        } else {
-            computerWins("scissors", "paper");
-        }
-        return;
-    }
-
-    if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            humanWins("rock", "scissors");
-        } else {
-            computerWins("paper", "rock");
-        }
-        return;
-    }
+let addLogEntry = (text) => {
+    const result = document.querySelector("#result");
+    const logEntry = document.createElement("p");
     
-    if (humanChoice === "scissors") {
-        if (computerChoice === "paper") {
-            humanWins("scissors", "paper");
-        } else {
-            computerWins("rock", "scissors");
-        }
-        return;
-    }
-    
-    console.log("Invalid human choice! Round cancelled");
-    return;
+    logEntry.textContent = text;
+    result.appendChild(logEntry);
 }
 
-let playGame = () => {
+let updateScoreUi = (computerScore = 0, humanScore = 0) => {
+    const scorePara = document.querySelector("#score");
+    scorePara.textContent = `Computer score: ${computerScore}, Human score: ${humanScore}`
+}
+
+function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+
+    const result = document.querySelector("#result");
+    const scorePara = document.createElement("p");
+    scorePara.id = "score";
+    scorePara.textContent = `Computer score: ${computerScore}, Human score: ${humanScore}`;
+    result.append(scorePara);
     
     let humanWins    = (winningPick, losingPick) => {
-        console.log(`You win! ${winningPick} beats ${losingPick}`);
+        addLogEntry(`You win! ${winningPick} beats ${losingPick}`);
         humanScore++;
     }
     let computerWins = (winningPick, losingPick) => {
-        console.log(`You lose! ${winningPick} beats ${losingPick}`);
+        addLogEntry(`You lose! ${winningPick} beats ${losingPick}`);
         computerScore++;
     };
+
+    let playRound = (humanChoice, computerChoice) => {
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        if (humanChoice === computerChoice) {
+            addLogEntry("It's a draw! No points were gained");
+            return;
+        }
+
+        if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                humanWins("paper", "rock");
+            } else {
+                computerWins("scissors", "paper");
+            }
+            return;
+        }
+
+        if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                humanWins("rock", "scissors");
+            } else {
+                computerWins("paper", "rock");
+            }
+            return;
+        }
+        
+        if (humanChoice === "scissors") {
+            if (computerChoice === "paper") {
+                humanWins("scissors", "paper");
+            } else {
+                computerWins("rock", "scissors");
+            }
+            return;
+        }
+        
+        addLogEntry("Invalid human choice! Round cancelled");
+        return;
+    }
     
-
-    // let ctr = 0;
-    // while (ctr < 5) {
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();    
-    //     playRound(humanSelection, computerSelection);
-    //     ctr++;
-    // }
-
-    // if (humanScore > computerScore) {
-    //     console.log("You win!");
-    // } else if (humanScore < computerScore) {
-    //     console.log("Computer wins!");
-    // } else {
-    //     console.log("It's a draw!");
-    // }
-}
-
-function init() {
     const select = document.querySelector("#selection");
-
     select.addEventListener("click", (event) => {
-        let target = event.target;
-        const result = document.querySelector("#result");
-        const userSelection = document.createElement("p");
+        const humanChoice = event.target.id;
 
-        switch (target.id) {
+        switch (humanChoice) {
             case "rock":
-                userSelection.textContent = "user selected rock";
+                addLogEntry("user selected rock");
                 break;
             case "paper":
-                userSelection.textContent = "user selected paper";
+                addLogEntry("user selected paper");
                 break;
             case "scissors":
-                userSelection.textContent = "user selected scissors";
+                addLogEntry("user selected scissors");
                 break;
         }
-        result.appendChild(userSelection);
         
-        playRound(target.id, getComputerChoice());
+        playRound(humanChoice, getComputerChoice());
+        updateScoreUi(computerScore, humanScore);
+
+        if (humanScore >= 5) {
+            console.log("You win!");
+            return;
+        }
+
+        if (computerScore >= 5) {
+            console.log("Computer wins!");
+            return;
+        }
+
     });
 }
 
-init();
 playGame();
